c7ff64fe974435cd6fdff7fe061be9aa1cea6dd1
Sam Willems <swi126@uclive.ac.nz>
2024-07-29 01:12:57 +1200

    changes to binds

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -7,9 +7,9 @@ return {
 		},
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>tg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>tt", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fr", builtin.buffers, {})
 		end,
 	},
 	{
--- neovim/lua/vim-options.lua
+++ neovim/lua/vim-options.lua
@@ -10,12 +10,6 @@ vim.o.cursorline = true
 vim.o.termguicolors = true
 vim.wo.number = true
 
--- Navigate vim panes better --
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
 vim.cmd("set nu")
 vim.cmd("set rnu")
 vim.cmd("set ma")
