eb5fc2f431c4db24ed77b8f2906be8af3d160677
Sam Willems <swi126@uclive.ac.nz>
2024-08-22 20:56:09 +1200

    Removed highlighting on search, added comments to vim-options

--- neovim/nvim/lua/vim-options.lua
+++ neovim/nvim/lua/vim-options.lua
@@ -1,7 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set expandtab")
 
 vim.opt.swapfile = false
 vim.g.mapleader = " "
@@ -10,14 +6,15 @@ vim.o.cursorline = true
 vim.o.termguicolors = true
 vim.wo.number = true
 
-vim.cmd("set nu")
-vim.cmd("set rnu")
-vim.cmd("set ma")
-
-vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set nu") -- Use line numbers
+vim.cmd("set rnu") -- Use relative numbers
+vim.cmd("set ma") -- Set state to modifiable
+vim.cmd("set clipboard+=unnamedplus") -- Set clipboard to use system clipboard
+vim.cmd("set nohlsearch") -- Don't use highlighting on search
+vim.cmd("set expandtab") -- Replace tabs with spaces
 
 vim.diagnostic.config({
-	virtual_text = false, -- Disable inline diagnostics
+	virtual_text = false, -- Disable inline diagnostics -- Replace tabs with spaces
 	signs = true, -- Keep signs in the gutter
 	underline = true, -- Keep underlining the text
 	update_in_insert = false, -- Don't show diagnostics in insert mode
