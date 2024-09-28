e01813717b39616b28f2b0088b26a2002fc316c8
Sam Willems <swi126@uclive.ac.nz>
2024-09-28 14:18:55 +1200

    add vim option ignorecase

--- neovim/nvim/lua/vim-options.lua
+++ neovim/nvim/lua/vim-options.lua
@@ -7,6 +7,7 @@ vim.opt.hlsearch = false                -- Don't use highlighting on search
 vim.opt.expandtab = true                -- Replace tabs with spaces
 vim.opt.tabstop = 4                     -- Set tab length to 4 spaces
 vim.opt.shiftwidth = 4                  -- Set indent length to 4 spaces
+vim.opt.ignorecase = true               -- Ignore search case
 vim.opt.smartcase = true                -- Ignore case on search unless search query contains capitals
 vim.opt.nu = true                       -- Show current line number
 vim.opt.rnu = true                      -- Use relative numbers
