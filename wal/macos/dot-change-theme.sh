b061a1fe1940af199f8a61a042069489c978ff52
Sam Willems <swi126@uclive.ac.nz>
2025-05-18 17:26:58 +1200

    Got script hot reloading working on mac

--- wal/macos/dot-change-theme.sh
+++ wal/macos/dot-change-theme.sh
@@ -5,3 +5,10 @@ IMAGE_LOCATION=$(< "$HOME/.cache/wal/wal")
 wallpaper set $IMAGE_LOCATION
 
 tmux source-file ~/.tmux.conf
+
+KITTY_SOCKET=$(find /tmp/mykitty-*)
+if [ -n "$KITTY_SOCKET" ]; then
+    kitty @ --to=unix:$KITTY_SOCKET set-colors --all --configured ~/.cache/wal/colors-kitty.conf
+else
+    echo "No Kitty socket found"
+fi
