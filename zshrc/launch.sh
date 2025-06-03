abb6aa7b2193f6116d0b64e34d6001ce7826a6e3
Sam Willems <swi126@uclive.ac.nz>
2025-06-03 13:55:38 +1200

    updated mac zsh launch script

--- zshrc/launch.sh
+++ zshrc/launch.sh
@@ -4,5 +4,11 @@ source ~/dotfiles/zshrc/launch_tmux.sh
    
 neofetch
 
-fortune -a | cowthink -f $(find /usr/share/cowsay/cows -type f | shuf -n 1)
+OS=$(uname)
+if [[ $OS == "Darwin" ]]; then
+    COW_PATH=$(brew --prefix cowsay)/share/cowsay/cows/
+    fortune | cowthink -f "$(find "$COW_PATH" -type f | shuf -n 1)"
+elif [[ $OS == "Linux" ]]; then
+    fortune -a | cowthink -f $(find /usr/share/cowsay/cows -type f | shuf -n 1)
+fi
 
