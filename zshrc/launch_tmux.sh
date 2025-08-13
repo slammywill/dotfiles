b3375d6f062b31815bb87e73333acb3b5f41fe5c
slammywill <samjwillems@protonmail.com>
2025-08-13 23:12:28 +0000

    changes

--- zshrc/launch_tmux.sh
+++ zshrc/launch_tmux.sh
@@ -4,6 +4,10 @@ in_ide() {
     [[ "$TERM_PROGRAM" == "vscode" ]] || [[ "$TERMINAL_EMULATOR" == "JetBrains-JediTerm" ]]
 }
 
-if [ -z "$TMUX" ] && ! in_ide; then
+has_attached_sessions() {
+    tmux list-sessions -F '#{session_attached}' 2>/dev/null | grep -q '^1$'
+}
+
+if [ -z "$TMUX" ] && ! in_ide && ! has_attached_sessions; then
     tmux attach || tmux new
 fi
